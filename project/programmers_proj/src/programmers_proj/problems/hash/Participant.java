8c1f2e6989329a667ec4442663054a4f3bf86719
zagood <ljbfif50@gmail.com>
2021-08-18 15:32:29 +0900

    백업

--- project/programmers_proj/src/programmers_proj/problems/hash/Participant.java
+++ project/programmers_proj/src/programmers_proj/problems/hash/Participant.java
@@ -23,36 +23,36 @@ public class Participant {
 		String answer = "";
 	    Map<String, Integer> answerMap = new HashMap<String, Integer>();
 	    
+	    /*------------ 기본 답안 ------------*/
+	    
+		/*
+		 * for(String part : participant){ answerMap.put(part,
+		 * answerMap.getOrDefault(part, 0) + 1); }
+		 * 
+		 * for(String comp : completion){ answerMap.put(comp, answerMap.get(comp) - 1);
+		 * }
+		 * 
+		 * for(String key : answerMap.keySet()){ if(answerMap.get(key) != 0 ){ answer =
+		 * key; break; } }
+		 */
+	    
+	    /*------------ stream을 활용한 답안 ------------*/
 	    Arrays.stream(participant).forEach(part -> {
 	    	answerMap.put(part, answerMap.getOrDefault(part, 0) + 1);
 	    });
 	    
-//	    for(String part : participant){
-//	        answerMap.put(part, answerMap.getOrDefault(part, 0) + 1);
-//	    }
-	    
 	    Arrays.stream(completion).forEach(comp -> {
 	    	answerMap.put(comp, answerMap.get(comp) - 1);
-		});
-	    
-//	    for(String comp : completion){
-//	        answerMap.put(comp, answerMap.get(comp) - 1);
-//	    }
+	    });
 	    
 	    Optional<String> findFirst = answerMap.entrySet().stream()
 	    		.filter(key -> key.getValue() != 0)
 	    		.map(Map.Entry::getKey)
 	    		.findFirst();
 	    
-	    answer = findFirst.get();
-	    //answerMap.keySet().stream().filter(answerMap.get(key) -> )
-	    
-//	    for(String key : answerMap.keySet()){
-//	        if(answerMap.get(key) != 0 ){
-//	            answer = key;
-//	            break; 
-//	        } 
-//	    }
+	    if(findFirst.isPresent()) {
+	    	answer = findFirst.get();
+	    }
 	    
 	    return answer;
 	}
