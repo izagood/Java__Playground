b13df7c26f49e0fb3e522d398db90a4769c2764d
zagood <ljbfif50@gmail.com>
2022-03-25 10:49:23 +0900

    enum 정의

--- workspace/SpringBootTaco/taco_cloud/src/test/java/tacos/Ingredient.java
+++ workspace/SpringBootTaco/taco_cloud/src/test/java/tacos/Ingredient.java
@@ -12,8 +12,29 @@ public class Ingredient {
 	private final Type type;
 	
 	public static enum Type {
-		WRAP("001", "랩"), PROTEIN("002"), VEGGIES("002"), CHEESE("002"), SAUCE("002"),;
 		
+		WRAP("001", "랩"), 
+		PROTEIN("002", "프로틴"), 
+		VEGGIES("003", "채소"), 
+		CHEESE("004", "치즈"), 
+		SAUCE("005", "소스"),
+		;
+		
+		private String code;
+		private String value;
+		
+		Type(String code, String value){
+			this.code = code;
+			this.value = value;
+		}
+		
+		public String getCode() {
+			return this.code;
+		}
+		
+		public String getValue() {
+			return this.value;
+		}
 		
 	}
 
